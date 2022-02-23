0d4496b30c7496d3e632ccbd48d09a79358ef274
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-23 11:35:15 -0500

    Added new boat form page with option to destroy a boat.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -14,8 +14,28 @@ class BoatsController < ApplicationController
   end
 
   def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    if @boat.save
+      redirect_to dashboard_my_boats_path
+    else
+      render :new
+    end
   end
 
   def new
+    @boat = Boat.new
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to dashboard_my_boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :make_model_year, :location, :price_per_day, :capacity, :description)
   end
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,7 +2,7 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :name, :make_model_year, :location, :price_per_day, :description, :capacity, :rating, presence: true
-  validates :price_per_day, :capacity, :rating, numericality: true
+  validates :name, :make_model_year, :location, :price_per_day, :description, :capacity, presence: true
+  validates :price_per_day, :capacity, numericality: true
   # validates :type, inclusion: { in: %w[small medium big huge] }
 end
