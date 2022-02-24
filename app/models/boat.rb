424f7412dcd8c8374bb780fa624a445f83b8da0f
Janaya Gripper <jnygripper@gmail.com>
2022-02-24 16:18:34 -0500

    added photos to browsing page and boat details page

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,8 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_one_attached :photo
-  # has_many_attached :photos
+  # has_one_attached :photo
+  has_many_attached :photos
 
   validates :name, :make_model_year, :location, :price_per_day, :description, :capacity, presence: true
   validates :price_per_day, :capacity, numericality: true
