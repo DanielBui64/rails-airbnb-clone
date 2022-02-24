0ee55baa779508f4047c0ab63cf4fd1ae1555812
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-24 12:29:51 -0500

    Created the dashboard page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    @user = current_user
+    @bookings_count = Booking.where(user: current_user).count
+    if current_user.owner
+      @booking_requests_count = Booking.where(boat: current_user.boats).count
+      @boats_count = Boat.where(user: current_user).count
+    end
   end
 
   def my_bookings
