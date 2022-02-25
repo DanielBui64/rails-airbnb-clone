30f8a424990070b8c1b7a59aa74787f6cd02700d
daniel bui <daniel-bui@hotmail.com>
2022-02-24 21:25:32 -0500

    added search bar to

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,9 +3,14 @@ class BoatsController < ApplicationController
     @boats = Boat.all
     @markers = @boats.geocoded.map do |boat|
       {
-        lat:boat.latitude,
-        lng:boat.longitude
-       }
+        lat: boat.latitude,
+        lng: boat.longitude,
+        info_window: render_to_string(
+          partial: "info_window",
+          locals: { boat: boat }
+        ),
+        image_url: helpers.asset_url("redboat.png")
+      }
     end
   end
 
